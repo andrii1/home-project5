371127f8efda15e09f9e3fb7a66e4d8f0583abf7
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2024-11-10 20:01:21 +0100

    wip

--- packages/client/src/sitemap-routes.js
+++ packages/client/src/sitemap-routes.js
@@ -3,16 +3,14 @@ import { Route } from 'react-router-dom';
 
 export default (
   <Route>
-    <Route path="/" />
-    <Route path="/apps" />
+    {/* <Route path="/" /> */}
+    {/* <Route path="/apps" /> */}
     <Route exact path="/numbergenerator/:numberMinParam/:numberMaxParam" />
     {/* <Route exact path="/apps/:id" />
     <Route exact path="/apps/topic/:topicIdParam" />
     <Route exact path="/apps/category/:categoryIdParam" /> */}
-    <Route exact path="/faq" />
+    {/* <Route exact path="/faq" /> */}
     <Route exact path="/login" />
     <Route exact path="/signup" />
-    <Route exact path="/reset" />
-    <Route exact path="/dashboard" />
   </Route>
 );
