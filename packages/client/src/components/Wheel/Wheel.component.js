d545191ebceedc09fbc12d9fce016fd529efd13d
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2024-11-12 18:14:41 +0100

    wip

--- packages/client/src/components/Wheel/Wheel.component.js
+++ packages/client/src/components/Wheel/Wheel.component.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { Button } from '../Button/Button.component';
@@ -52,12 +52,6 @@ export const Wheel = ({ participants }) => {
     document.body.style.overflow = 'visible';
   };
 
-  useEffect(() => {
-    if (canvasRef.current) {
-      drawWheel();
-    }
-  }, [participants, rotation]);
-
   const darkenColor = (color, amount) => {
     let r = parseInt(color.slice(1, 3), 16);
     let g = parseInt(color.slice(3, 5), 16);
@@ -71,7 +65,7 @@ export const Wheel = ({ participants }) => {
     return `#${((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)}`;
   };
 
-  function drawWheel() {
+  const drawWheel = useCallback(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     const radius = canvas.width / 2;
@@ -126,7 +120,13 @@ export const Wheel = ({ participants }) => {
     ctx.fillStyle = 'red';
     ctx.fill();
     ctx.restore();
-  }
+  }, [numSectors, participants, rotation]);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      drawWheel();
+    }
+  }, [participants, rotation, drawWheel]);
 
   const startSpin = () => {
     if (spinning) return;
@@ -143,6 +143,7 @@ export const Wheel = ({ participants }) => {
     const spinDuration = 6000;
     const easing = (t) => {
       // Ease-out cubic
+      // eslint-disable-next-line prefer-exponentiation-operator
       return 1 - Math.pow(1 - t, 3);
     };
 
