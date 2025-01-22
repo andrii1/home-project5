c4035c5db1a0459751e7a88f611c5ef9b34ad039
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-01-22 16:40:12 +0100

    wip

--- packages/client/src/containers/Home/Home.Container.js
+++ packages/client/src/containers/Home/Home.Container.js
@@ -6,17 +6,32 @@ import { apiURL } from '../../apiURL';
 import { Button } from '../../components/Button/Button.component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+const tools = [
+  { id: 1, title: 'Number Generator', url: 'numbergenerator' },
+  { id: 2, title: 'Random number wheel', url: 'random-number-wheel' },
+  { id: 3, title: 'List randomizer wheel', url: 'list-randomizer-wheel' },
+  {
+    id: 4,
+    title: 'Random color generator React',
+    url: 'random-color-generator-react',
+  },
+  {
+    id: 5,
+    title: 'Random QR code',
+    url: 'random-qr-code',
+  },
+  {
+    id: 6,
+    title: 'Star rating',
+    url: 'star-rating',
+  },
+  {
+    id: 7,
+    title: 'GitHub profile search',
+    url: 'github-profile-search',
+  },
+];
 export const Home = () => {
-  const tools = [
-    { id: 1, title: 'Number Generator', url: 'numbergenerator' },
-    { id: 2, title: 'Random number wheel', url: 'random-number-wheel' },
-    { id: 3, title: 'List randomizer wheel', url: 'list-randomizer-wheel' },
-    {
-      id: 4,
-      title: 'Random color generator React',
-      url: 'random-color-generator-react',
-    },
-  ];
   const cardItems = tools.map((tool) => {
     return (
       <Link to={tool.url}>
