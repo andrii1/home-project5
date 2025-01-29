10fad2ed28845cdd1168ca2bf8c787e7ddad7a3b
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-01-29 17:51:51 +0100

    wip

--- packages/client/src/containers/EyeGymnastics/EyeGymnastics.Container.js
+++ packages/client/src/containers/EyeGymnastics/EyeGymnastics.Container.js
@@ -3,16 +3,19 @@ import React, { useEffect, useState, useRef } from 'react';
 
 import { Helmet } from 'react-helmet';
 import './EyeGymnastics.Style.css';
+import { Button } from '../../components/Button/Button.component';
 
-let radius = 50;
-let vx = 7;
-let vy = 7;
+const radius = 50;
+let vx = 5;
+let vy = 5;
 let move;
 
 export const EyeGymnastics = () => {
-  let canvasRef = useRef(null);
+  const [directionHorizontal, setDirectionHorizontal] = useState(false);
+
+  const canvasRef = useRef(null);
   useEffect(() => {
-    let context = canvasRef.current.getContext('2d');
+    const context = canvasRef.current.getContext('2d');
     let x = canvasRef.current.width / 2;
     let y = canvasRef.current.height / 2;
     const drawBall = () => {
@@ -27,23 +30,53 @@ export const EyeGymnastics = () => {
       context.fillStyle = '#b5838d';
       context.fill();
 
-      // x += vx;
-      y += vy;
-
-      //  if (x > canvasRef.current.width - radius || x < radius) {
-      //    vx *= -1;
-      //  } else
-
-      if (y > canvasRef.current.height - radius || y < radius) {
-        vy *= -1;
+      if (directionHorizontal) {
+        x += vx;
+        if (x > canvasRef.current.width - radius || x < radius) {
+          vx *= -1;
+        }
+      } else if (!directionHorizontal) {
+        y += vy;
+        if (y > canvasRef.current.height - radius || y < radius) {
+          vy *= -1;
+        }
       }
       move = requestAnimationFrame(drawBall);
     };
+
     drawBall();
+
     return () => {
       cancelAnimationFrame(move);
     };
-  });
+  }, [directionHorizontal]);
+
+  useEffect(() => {
+    // Implementing the setInterval method
+    const interval = setInterval(() => {
+      setDirectionHorizontal(!directionHorizontal);
+    }, 9000);
+
+    // Clearing the interval
+    return () => clearInterval(interval);
+  }, [directionHorizontal]);
+
+  // timeOut functionality
+  // useEffect(() => {
+  //   // Use setTimeout to update the message after 2000 milliseconds (2 seconds)
+  //   const timeoutId = setTimeout(() => {
+  //     setIsRunning(false);
+  //   }, 3000);
+
+  //   // Cleanup function to clear the timeout if the component unmounts
+  //   return () => clearTimeout(timeoutId);
+  // }, []);
+
+  // const handleClick = () => {
+  //   setTimeout(() => {
+
+  //   }, 3000);
+  // };
 
   return (
     <main>
@@ -54,14 +87,20 @@ export const EyeGymnastics = () => {
       {/* <div className="hero"></div> */}
       <div className="hero">
         <h1 className="hero-header">Eye gymnastics</h1>
-        <p className="subheading">Follow the ball with your eyes</p>
+        <p className="subheading">
+          Follow the ball with your eyes (
+          {directionHorizontal ? 'left to right' : 'up-down'})
+        </p>
+        {/* <Button primary onClick={handleClick}>
+          Start
+        </Button> */}
       </div>
       <section className="container-tool">
         <canvas
           ref={canvasRef}
           className="canvas-eye-gymnastics"
           width={window.innerWidth - 50}
-          height={window.innerHeight - 50}
+          height={window.innerHeight - 300}
         >
           1
         </canvas>
