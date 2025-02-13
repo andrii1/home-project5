f56af32ebff2d280066a71035792a2e0cd846c88
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-02-13 15:23:22 +0100

    wip

--- packages/client/src/containers/GitHubStats/GitHubStats.Container.js
+++ packages/client/src/containers/GitHubStats/GitHubStats.Container.js
@@ -26,12 +26,14 @@ export const GitHubStats = () => {
   const [githubData, setGithubData] = useState();
   const [error, setError] = useState();
   const [loading, setLoading] = useState(false);
+  const [mode, setMode] = useState('2d');
+  const [imgUrl, setImgUrl] = useState(undefined);
 
   const fetchData = async (param) => {
     setLoading(true);
     try {
       const response = await fetch(
-        `https://github-contributions-api.deno.dev/andrii1.svg?from=2023-11-11`,
+        `https://github-contributions-api.deno.dev/${param}.json`,
       );
       const data = await response.json();
 
@@ -53,12 +55,18 @@ export const GitHubStats = () => {
 
       // OCTOKIT END
 
+      //https://github-contributions-api.deno.dev/${param}.json?from=2023-11-11
+
       console.log(data);
 
-      // if (!response.ok) {
-      //   throw new Error(data.message || 'Failed to fetch');
-      // }
-      setGithubData(data);
+      if (!response.ok) {
+        throw new Error(data.message || 'Failed to fetch');
+      }
+      if (mode === '2d') {
+        setImgUrl(`https://github-contributions-api.deno.dev/${param}.svg`);
+      } else {
+        setGithubData(data);
+      }
       setError(null);
     } catch (e) {
       setError({ message: e.message || 'An error occured' });
@@ -67,6 +75,8 @@ export const GitHubStats = () => {
   };
 
   const handleSearch = () => {
+    setImgUrl(null);
+    setGithubData(null);
     fetchData(search);
     setSearch('');
   };
@@ -83,8 +93,10 @@ export const GitHubStats = () => {
   //   );
   // });
 
+  console.log(mode);
+
   return (
-    <main>
+    <main className="single-app-container">
       <Helmet>
         <title>GitHub contribution chart</title>
         <meta name="description" content={keywords && keywords.join(', ')} />
@@ -92,35 +104,46 @@ export const GitHubStats = () => {
       <div className="hero">
         <h1 className="hero-header">GitHub contribution chart</h1>
       </div>
-      <div className="search-container github">
+      <section className="search-container github">
         <div className="search-input-container">
           <div className="radio-group">
-            <Radio label="2d" />
-            <Radio label="3d graph" />
+            <Radio
+              value="2d"
+              label="2d"
+              onChange={(event) => setMode(event.target.value)}
+              checked={mode === '2d'}
+            />
+            <Radio
+              value="3d"
+              label="3d graph"
+              onChange={(event) => setMode(event.target.value)}
+              checked={mode === '3d'}
+            />
           </div>
           <TextFormInput
             value={search}
-            placeholder="Find recipes"
+            placeholder="Enter GitHub username"
             onChange={setSearch}
           />
-          <Button onClick={handleSearch} primary label="Search" />
+          <Button onClick={handleSearch} primary label="Create chart" />
         </div>
-      </div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          {error && <p className="error-message">{error.message}</p>}
-          {githubData && !error && (
-            <img
-              src="https://github-contributions-api.deno.dev/andrii1.svg?from=2023-11-11"
-              alt="GitHub contribution chart"
-              width="80"
-            />
-            // <div className="container-cards">{githubData}</div>
-          )}
-        </>
-      )}
+      </section>
+      <section className="app-result-container">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            {error && <p className="error-message">{error.message}</p>}
+            {githubData && !error && (
+              <div>3d</div>
+              // <div className="container-cards">{githubData}</div>
+            )}
+            {imgUrl && !error && (
+              <img src={imgUrl} alt="GitHub contribution chart" width="700" />
+            )}
+          </>
+        )}
+      </section>
     </main>
   );
 };
