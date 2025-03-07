084d403351ad54f6307d55aa0d0981957412cb60
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-03-07 15:56:15 +0100

    wip

--- packages/client/src/containers/RandomQrCode/RandomQrCode.Container.js
+++ packages/client/src/containers/RandomQrCode/RandomQrCode.Container.js
@@ -7,6 +7,19 @@ import './RandomQrCode.Style.css';
 import QRCode from 'react-qr-code';
 import TextFormInput from '../../components/Input/TextFormInput.component';
 
+const keywords = [
+  'random qr code',
+  'random qr',
+  'random qr generator',
+  'qr code random',
+  'random qr codes',
+  'random q r codes',
+  'qr codes random',
+  'generate random qr code',
+  'random qr codes to scan',
+  'random qr code maker',
+];
+
 export const RandomQrCode = () => {
   const [input, setInput] = useState('');
   const [qrCode, setQrCode] = useState('');
@@ -20,7 +33,7 @@ export const RandomQrCode = () => {
     <main>
       <Helmet>
         <title>Random QR Code</title>
-        <meta name="description" content="Random q r code" />
+        <meta name="description" content={keywords && keywords.join(', ')} />
       </Helmet>
       <div className="hero">
         <h1 className="hero-header">Random QR code</h1>
