b2e21cb7eb4e8f0db067fc46df56a0312355a779
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-10-02 18:10:15 +0200

    wip

--- packages/server/api/lib/utils/createBlogsMrhack/serpApi.js
+++ packages/server/api/lib/utils/createBlogsMrhack/serpApi.js
@@ -18,10 +18,38 @@ const excludeList = [
   'auto',
   'state',
 ];
+
+const serpCategories = [
+  // { id: 0, name: 'All categories' },
+  { id: 3, name: 'Arts & Entertainment' },
+  { id: 5, name: 'Computers & Electronics' },
+  { id: 7, name: 'Finance' },
+  { id: 8, name: 'Games' },
+  { id: 11, name: 'Home & Garden' },
+  { id: 12, name: 'Business & Industrial' },
+  { id: 13, name: 'Internet & Telecom' },
+  { id: 14, name: 'People & Society' },
+  { id: 16, name: 'News' },
+  { id: 18, name: 'Shopping' },
+  { id: 19, name: 'Law & Government' },
+  { id: 20, name: 'Sports' },
+  { id: 22, name: 'Books & Literature' },
+  { id: 23, name: 'Performing Arts' },
+  { id: 24, name: 'Visual Art & Design' },
+  { id: 25, name: 'Advertising & Marketing' },
+  { id: 28, name: 'Office Services' },
+  { id: 29, name: 'Real Estate' },
+  { id: 30, name: 'Computer Hardware' },
+  { id: 1075, name: 'Courts & Judiciary' },
+  { id: 1076, name: 'Signage' },
+  { id: 1077, name: 'Sports News' },
+];
+
 // const seedList = ["maker", "template", "generator", "calculator", "tool"];
 const seedList = ['app', 'error', 'website', 'app iphone', 'widget'];
+const seedListApp = ['app'];
 
-async function fetchSerpApi(seedParam, periodParam) {
+async function fetchSerpApi(seedParam, periodParam, categoryParam) {
   const params = new URLSearchParams({
     engine: 'google_trends',
     q: seedParam,
@@ -31,6 +59,10 @@ async function fetchSerpApi(seedParam, periodParam) {
     data_type: 'RELATED_QUERIES',
     api_key: SERP_API_KEY,
   });
+
+  if (categoryParam) {
+    params.set('cat', categoryParam);
+  }
 
   try {
     const response = await fetch(`https://serpapi.com/search?${params}`);
@@ -52,11 +84,20 @@ async function fetchSerpApi(seedParam, periodParam) {
   }
 }
 
-const useAllQueries = async (period) => {
+const useAllQueries = async (period, withCategories = false) => {
   const allQueries = [];
-  for (const seed of seedList) {
-    const result = await fetchSerpApi(seed, period);
-    allQueries.push(...result);
+  if (withCategories) {
+    for (const seed of seedListApp) {
+      for (const category of serpCategories) {
+        const result = await fetchSerpApi(seed, period, category.id);
+        allQueries.push(...result);
+      }
+    }
+  } else {
+    for (const seed of seedList) {
+      const result = await fetchSerpApi(seed, period);
+      allQueries.push(...result);
+    }
   }
   console.log(allQueries);
 
