b6cc4613ca9aae4e8f6ca47356398f71d4ff1ecd
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-10-05 18:21:06 +0200

    wip

---
+++ packages/server/migrations/20251005161846_add_highlighted_queriesMrhack.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('queriesMrhack', (table) => {
+    table.boolean('highlighted').defaultTo('false');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('queriesMrhack', (table) => {
+    table.dropColumn('highlighted');
+  });
+};
