374110fde6902ad80a5fd7807e2038555a30639c
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-11-21 15:06:09 +0100

    wip

--- packages/server/api/lib/utils/updateQueriesMrhack.js
+++ packages/server/api/lib/utils/updateQueriesMrhack.js
@@ -1,9 +1,15 @@
 const knex = require('../../../config/db');
+require('dotenv').config();
+
+const API_PATH = process.env.API_PATH_DEALS_PROD;
 
 async function run() {
   try {
     // 1. Fetch all rows from queries
-    const rows = await knex('queries');
+    // const rows = await knex('queries');
+
+    const response = await fetch(`${API_PATH}/queries`);
+    const rows = await response.json();
 
     if (!rows.length) {
       console.log('No rows found in queries.');
