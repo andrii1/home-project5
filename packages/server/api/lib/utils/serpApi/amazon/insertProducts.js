10b4d71654e24bce8256344407e596e860c0fc1f
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2026-03-04 11:19:06 +0100

    wip

---
+++ packages/server/api/lib/utils/serpApi/amazon/insertProducts.js
@@ -0,0 +1,116 @@
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable import/no-extraneous-dependencies */
+require('dotenv').config();
+const fetchSerpApiAmazon = require('../serpApiAmazon');
+
+// Credentials (from .env)
+const USER_UID = process.env.USER_UID_AMAZON_PROD;
+const API_PATH = process.env.API_PATH_AMAZON_PROD;
+
+// fetch helpers
+
+const today = new Date();
+const todayDay = today.getDay(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+
+const allowedDays = [0, 1, 2, 3, 4, 5];
+
+if (!allowedDays.includes(todayDay)) {
+  console.log('Not an allowed day, skipping job.');
+  process.exit(0);
+}
+
+async function insertCategory(title, categoryNodeId) {
+  const res = await fetch(`${API_PATH}/categories`, {
+    method: 'POST',
+    headers: {
+      token: `token ${USER_UID}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ title, nodeId: categoryNodeId }),
+  });
+  const data = await res.json();
+  return data; // assume it returns { id, full_name }
+}
+
+async function insertProduct({
+  title,
+  asin,
+  price,
+  rating,
+  reviews,
+  url,
+  url_affiliate,
+  url_serpapi,
+  url_image,
+  categoryId,
+}) {
+  const body = {
+    title,
+    asin,
+    price,
+    rating,
+    reviews,
+    url,
+    url_affiliate,
+    url_serpapi,
+    url_image,
+    category_id: categoryId,
+  };
+
+  const res = await fetch(`${API_PATH}/products/node`, {
+    method: 'POST',
+    headers: {
+      token: `token ${USER_UID}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  const data = await res.json();
+  return data; // assume it returns { id, full_name }
+}
+
+const insertProducts = async () => {
+  // console.log(appsParam);
+
+  let products;
+  if (allowedDays.includes(todayDay)) {
+    products = await fetchSerpApiAmazon();
+  }
+
+  for (const product of products) {
+    try {
+      const category = product.categoryTitle;
+      const { categoryNodeId } = product;
+
+      const newCategory = await insertCategory(category, categoryNodeId);
+      const { categoryId } = newCategory;
+      console.log('Inserted category:', newCategory);
+
+      const newProduct = await insertProduct({
+        title: product.title,
+        asin: product.asin,
+        price: product.price,
+        rating: product.rating,
+        reviews: product.reviews,
+        url: product.url,
+        url_affiliate: product.url_affiliate,
+        url_serpapi: product.url_serpapi,
+        url_image: product.url_image,
+        categoryId,
+      });
+      const { productId } = newProduct;
+      const newProductTitle = newProduct.productTitle;
+      console.log('Inserted product:', newProduct);
+    } catch (err) {
+      console.error(
+        `❌ Failed to insert product ${product.asin}:`,
+        err.message,
+      );
+      // continue with next app
+    }
+  }
+};
+
+insertProducts().catch(console.log);
---
+++ packages/server/api/lib/utils/serpApi/serpApiAmazon.js
@@ -0,0 +1,171 @@
+/* eslint-disable no-console */
+/* eslint-disable no-await-in-loop */
+/* eslint-disable no-restricted-syntax */
+require('dotenv').config();
+
+const {
+  SERP_API_KEY,
+  SERP_API_KEY2,
+  SERP_API_KEY3,
+  SERP_API_KEY4,
+  SERP_API_KEY5,
+  SERP_API_KEY6,
+} = process.env;
+
+const apiKeys = [
+  SERP_API_KEY,
+  SERP_API_KEY2,
+  SERP_API_KEY3,
+  SERP_API_KEY4,
+  SERP_API_KEY5,
+  SERP_API_KEY6,
+];
+
+let currentKeyIndex = 0;
+
+const excludeList = [
+  'insurance',
+  'cars',
+  'mortgage',
+  'loan',
+  'renters',
+  'vehicle',
+  'mutual',
+  'cheap',
+  'auto',
+  'state',
+];
+
+const amazonCategories = [
+  // { title: 'Appliances', nodeId: '2619526011' },
+  // { title: 'Arts, Crafts & Sewing', nodeId: '2617942011' },
+  // { title: 'Automotive', nodeId: '15690151' },
+  // { title: 'Baby', nodeId: '165797011' },
+  // { title: 'Beauty', nodeId: '11055981' },
+  // { title: 'Books', nodeId: '1000' },
+  // { title: 'Collectibles & Fine Arts', nodeId: '4991426011' },
+  // { title: 'Electronics', nodeId: '493964' },
+  // { title: 'Clothing, Shoes & Jewelry', nodeId: '7141124011' },
+  // { title: 'Gift Cards', nodeId: '2864120011' },
+  // { title: 'Grocery & Gourmet Food', nodeId: '16310211' },
+  // { title: 'Handmade', nodeId: '11260433011' },
+  // { title: 'Health & Personal Care', nodeId: '3760931' },
+  // { title: 'Home & Kitchen', nodeId: '1063498' },
+  // { title: 'Industrial & Scientific', nodeId: '16310161' },
+  // { title: 'Kindle Store', nodeId: '133141011' },
+  // { title: 'Patio, Lawn & Garden', nodeId: '3238155011' },
+  // { title: 'Luggage & Travel Gear', nodeId: '9479199011' },
+  // { title: 'Magazine Subscriptions', nodeId: '599872' },
+  // { title: 'Apps & Games', nodeId: '2350150011' },
+  // { title: 'Movies & TV', nodeId: '2625374011' },
+  // { title: 'Digital Music', nodeId: '624868011' },
+  // { title: 'CDs & Vinyl', nodeId: '301668' },
+  // { title: 'Musical Instruments', nodeId: '11965861' },
+  // { title: 'Office Products', nodeId: '1084128' },
+  // { title: 'Computers', nodeId: '541966' },
+  // { title: 'Pet Supplies', nodeId: '2619534011' },
+  // { title: 'Software', nodeId: '409488' },
+  // { title: 'Sports & Outdoors', nodeId: '3375301' },
+  // { title: 'Tools & Home Improvement', nodeId: '468240' },
+  // { title: 'Toys & Games', nodeId: '165795011' },
+  // { title: 'Vehicles', nodeId: '10677470011' },
+  // { title: 'Video Games', nodeId: '11846801' },
+  // { title: 'Wine', nodeId: '2983386011' },
+  { title: 'Cell Phones & Accessories', nodeId: '2335753011' },
+];
+
+function extractAmazonRawProducts(data) {
+  const keywords = ['best_sellers', 'featured', 'deals', 'organic_results'];
+
+  const collected = [];
+
+  for (const [key, value] of Object.entries(data)) {
+    if (!Array.isArray(value)) continue;
+
+    const matchesKeyword = keywords.some((keyword) =>
+      key.toLowerCase().includes(keyword),
+    );
+
+    if (matchesKeyword) {
+      collected.push(...value);
+    }
+  }
+
+  return collected;
+}
+
+function extractAmazonProducts(data) {
+  if (data.error) {
+    console.log('SerpAPI error:', data.error);
+    return [];
+  }
+
+  if (data.search_information?.organic_results_state === 'Fully empty') {
+    console.log('Amazon returned empty results');
+    return [];
+  }
+
+  const rawProducts = extractAmazonRawProducts(data);
+
+  return rawProducts.map((item) => ({
+    asin: item.asin,
+    title: item.title,
+    price: item.extracted_price || item.price || null,
+    rating: item.rating || null,
+    reviews: item.reviews || null,
+    url: item.link_clean,
+    url_affiliate: `${item.link_clean}?tag=onlinehikes-20`,
+    url_serpapi: item.serpapi_link,
+    url_image: item.thumbnail,
+  }));
+}
+
+async function fetchSerpApiAmazon(
+  categoryId,
+  categoryTitle,
+  domain = 'amazon.com',
+) {
+  const apiKey = apiKeys[currentKeyIndex];
+  currentKeyIndex = (currentKeyIndex + 1) % apiKeys.length;
+
+  const params = new URLSearchParams({
+    engine: 'amazon',
+    amazon_domain: domain,
+    type: 'best_sellers',
+    node: categoryId,
+    api_key: apiKey,
+  });
+
+  try {
+    const response = await fetch(`https://serpapi.com/search.json?${params}`);
+    const data = await response.json();
+    console.log('data', data);
+
+    const products = extractAmazonProducts(data).map((product) => ({
+      ...product,
+      categoryNodeId: categoryId,
+      categoryTitle: categoryTitle,
+    }));
+
+    console.log('products1', products);
+    return products;
+  } catch (error) {
+    console.error('Error fetching Amazon best sellers:', error);
+    return [];
+  }
+}
+
+const useAmazon = async () => {
+  const allProducts = [];
+
+  for (const category of amazonCategories) {
+    const products = await fetchSerpApiAmazon(category.nodeId, category.title);
+    allProducts.push(...products);
+  }
+  console.log('allProducts', allProducts);
+
+  return allProducts;
+};
+
+useAmazon().catch(console.error);
+module.exports = useAmazon;
