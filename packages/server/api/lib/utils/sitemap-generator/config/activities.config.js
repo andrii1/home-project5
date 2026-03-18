b4280cb92fb71a02535bedb35c606b0535478e81
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2026-03-18 17:53:32 +0100

    wip

---
+++ packages/server/api/lib/utils/sitemap-generator/config/activities.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  host: 'https://www.booktravelactivities.com/',
+  folder: 'activities',
+  staticRoutes: ['/', '/products'],
+  apiPaths: [
+    { path: 'products', prefix: 'products' },
+    { path: 'categories', prefix: 'products/categories' },
+    { path: 'countries', prefix: 'products/countries' },
+    { path: 'areas', prefix: 'products/areas' },
+    { path: 'cities', prefix: 'products/cities' },
+    { path: 'tags', prefix: 'products/tags' },
+    { path: 'highlights', prefix: 'products/highlights' },
+    { path: 'useCases', prefix: 'products/useCases' },
+    { path: 'userTypes', prefix: 'products/userTypes' },
+    { path: 'occasions', prefix: 'products/occasions' },
+  ],
+  maxUrls: 10000,
+};
