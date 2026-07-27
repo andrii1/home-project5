114cad0e807ae2224948112ec9a7dfc2b62b2e34
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2026-07-27 16:57:49 +0200

    wip

--- packages/client/src/components/Footer/Footer.component.js
+++ packages/client/src/components/Footer/Footer.component.js
@@ -5,34 +5,36 @@ import { Donate } from '../Donate/Donate.component';
 
 export const Footer = () => {
   return (
-    <div className="footer">
-      <Donate />
-      <div className="menu">
-        <ul>
-          <li>
-            <a
-              href="https://www.buymeacoffee.com/mrhackio"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              ☕ Buy Me a Coffee
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://paypal.me/museuly"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              💙 Support with PayPal
-            </a>
-            {/* <NavLink to="/faq" className="nav-link">
+    <div className="site-footer">
+      <div className="footer-container">
+        <Donate />
+        <div className="menu">
+          <ul>
+            <li>
+              <a
+                href="https://www.buymeacoffee.com/mrhackio"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ☕ Buy Me a Coffee
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://paypal.me/museuly"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                💙 Support with PayPal
+              </a>
+              {/* <NavLink to="/faq" className="nav-link">
               FAQ
             </NavLink> */}
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
+        <span>&copy;2026</span>
       </div>
-      <span>&copy;2026</span>
     </div>
   );
 };
