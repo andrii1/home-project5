783d95547dd100b49f2a42f22c63f3e5a6812e84
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2026-07-28 18:48:06 +0200

    wip

--- packages/server/api/lib/utils/insertScripts/insertAnswers.js
+++ packages/server/api/lib/utils/insertScripts/insertAnswers.js
@@ -4,45 +4,87 @@
 require('dotenv').config();
 const knex = require('../../../../config/db');
 
-const chapterId = 22;
+const chapterIdRei = 23;
 
-const answerTitles = [
-  'Vincent',
-  'Errands',
-  'Not planning to drink',
-  '0814',
-  'Walk',
-  'Prescription',
-  'You can talk',
-  'Photos',
-  'Draft',
-  'Dr. Marcus Adler',
-  'Lawyer',
-  'iMessage',
-  'Tucker',
-  'Water',
-  'Tim Piazza',
-  '1:32 am',
-  'BAC only',
-  'Walk in for me',
-  'Tucker',
-  'Tucker & Marcus',
+const answerTitlesRei = [
+  'May 17',
+  'spark',
+  'Check the studio',
+  'none',
+  'Injections',
+  'mother signed',
+  'Switch',
+  'Han Jiwon',
+  'AETHER_practice_backup',
+  'Voice memos',
+  'Cafe Vert',
+  'Kang',
+  'Confidential',
+  'Sign',
+  'Choi',
+  'Madame park',
+  'Hi',
+  'school',
+  'File',
+  'Director Park & Madame Park',
 ];
 
-async function insertChapterAnswers() {
+const chapterIdDanny = 24;
+
+const answerTitlesDanny = [
+  'Daniel',
+  'Sebastian',
+  'liar',
+  '6',
+  'Romford',
+  'Eyes',
+  'Alessandro Conti',
+  'Frozen',
+  'nadia',
+  'Vale',
+  '74000',
+  'Gift',
+  'Definitely',
+  'G. Cole',
+  'Vault',
+  'Yacht photo',
+  'Police',
+  'Deal',
+  'Wathamstow',
+  'Gemma Cole, Daniel Cole',
+];
+
+// async function insertChapterAnswers() {
+//   const questions = await knex('questions')
+//     .where({ chapter_id: chapterId })
+//     .orderBy('id');
+
+//   const answers = questions.map((q, index) => ({
+//     title: answerTitles[index],
+//     question_id: q.id, // or q.question_id depending on schema
+//     user_id: 1,
+//   }));
+
+//   await knex('answers').insert(answers);
+
+//   console.log(`Inserted ${answers.length} answers for chapter ${chapterId}`);
+// }
+
+async function insertChapterAnswers(chapterParam, answersParam) {
   const questions = await knex('questions')
-    .where({ chapter_id: chapterId })
+    .where({ chapter_id: chapterParam })
     .orderBy('id');
 
   const answers = questions.map((q, index) => ({
-    title: answerTitles[index],
+    title: answersParam[index],
     question_id: q.id, // or q.question_id depending on schema
     user_id: 1,
   }));
 
   await knex('answers').insert(answers);
 
-  console.log(`Inserted ${answers.length} answers for chapter ${chapterId}`);
+  console.log(`Inserted ${answers.length} answers for chapter ${chapterParam}`);
 }
 
-insertChapterAnswers();
+insertChapterAnswers(chapterIdRei, answerTitlesRei);
+insertChapterAnswers(chapterIdDanny, answerTitlesDanny);
